02e2ff681074a49d6980f399e2a782e99e547ed1
rafaeld108 <38574440+rafaeld108@users.noreply.github.com>
2023-06-19 23:52:41 -0300

    criado o model de exemplo, onde será responsável por trazer os dados do db

---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization{
+    /**
+     * id da organizaçaão
+     * @var integer
+     */
+    public $id = 1;
+    /**
+     * nom da organização
+     * @var string
+     */
+    public $name = 'Organização DevSystem';
+    /**
+     * site da organização
+     * @var string
+     */
+    public $site = 'https://DevSystem.com.br';
+    /**
+     * descrição da organização
+     * @var string
+     */
+    public $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, eum. Numquam, quos earum! Fuga delectus autem at, corrupti quod doloremque quam consectetur voluptatem quibusdam sunt? Recusandae fuga vel sit veniam.";
+}
